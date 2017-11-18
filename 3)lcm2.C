cc04447749ae9276bb6289c6fdfd2e4fdfde554b
VIJAY158 <30854995+VIJAY158@users.noreply.github.com>
2017-11-18 21:29:29 +0530

    Create 3)lcm2.C

---
+++ 3)lcm2.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int n1,n2,L;
+printf("Enter the no's");
+scanf("%d%d",&n1,&n2);
+L=(n1>n2) ? n1 : n2;
+while(1)
+{
+if((L%n1==0) && (L%n2==0))
+{
+printf("Lcm of the (%d,%d) is %d ",n1,n2,L);
+break;
+}
+L++;
+}
+}
